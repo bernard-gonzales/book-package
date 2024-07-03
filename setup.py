5c2b71f2c183e40c77ddc3d87f07590619628bf7
Bernard Gonzales <bernardgonzales125@gmail.com>
2024-07-03 09:14:39 -0400

    Homework 9

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='book-package',
+    name='bookpackage',
     version='0.1',
     description='A small package',
-    url='https://github.com/bernard-gonzales/book-package',
+    url='https://github.com/bernard-gonzales/bookpackage',
     author='Bernard Gonzales',
     author_email='bernardgonzales125@gmail.com',
     license='MIT',
-    packages=['book-lover']
+    packages=['booklover']
 )
